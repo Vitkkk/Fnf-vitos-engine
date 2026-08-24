569d8207d872cd150d4bb643cc9d93b8cee3a493
Vitkkk <asstecbp@gmail.com>
2026-08-24 14:53:18 -0300

    Bump Android build to 0.5.0 alpha05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.vitkkk.fnfmobilestudio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0-alpha04"
+        versionCode = 5
+        versionName = "0.5.0-alpha05"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
