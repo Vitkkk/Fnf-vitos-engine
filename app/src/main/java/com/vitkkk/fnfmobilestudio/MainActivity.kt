e5ae6cfad9f6d1852acafadca13f50492d2f8c13
Vitkkk <asstecbp@gmail.com>
2026-08-23 22:58:42 -0300

    Launch visual selector and stage editor flow

--- app/src/main/java/com/vitkkk/fnfmobilestudio/MainActivity.kt
+++ app/src/main/java/com/vitkkk/fnfmobilestudio/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.vitkkk.fnfmobilestudio.storage.ProjectStore
-import com.vitkkk.fnfmobilestudio.ui.StudioApp
+import com.vitkkk.fnfmobilestudio.ui.StudioAppV3
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val projectStore = ProjectStore(applicationContext)
         setContent {
-            StudioApp(projectStore)
+            StudioAppV3(projectStore)
         }
     }
 }
