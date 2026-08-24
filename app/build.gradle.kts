469f30a1aefbeb233cfd81daa2d66f391d00b59d
Vitkkk <asstecbp@gmail.com>
2026-08-23 23:32:13 -0300

    Bump Android build to 0.4.0 alpha04

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.vitkkk.fnfmobilestudio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0-alpha03"
+        versionCode = 4
+        versionName = "0.4.0-alpha04"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
